c1db52ae43a171766a09dc4ab0ed88af8493b109
kris2kulbida <91053835+kris2kulbida@users.noreply.github.com>
2021-11-26 16:58:58 +0200

    Add files via upload

---
+++ 2 проект.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    char c;
+    string s, s1;
+    
+    cin >> c;
+    cin >> s >> s1;
+    
+    for(int i = 0; i < s.length(); ++i) {
+        if(s[i] == c) {
+            s.insert(i, s1);
+            
+            i += s1.length();
+        }
+    }
+    
+    cout << s;
+    
+    system("pause");
+    return 0;
+}
+
